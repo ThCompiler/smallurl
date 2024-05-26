456c8b26c621d248c9b3c0f066ecf2b46a7f5474
Artem Vetoshkin <vet_v2002@mail.ru>
2024-05-26 21:09:48 +0300

    Delete script directory

--- script/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS urls
-(
-    id        bigserial   not null primary key,
-    long_url  text        not null,
-    short_url varchar(10) not null unique
-);
-
-CREATE INDEX short_urls on urls (short_url);
